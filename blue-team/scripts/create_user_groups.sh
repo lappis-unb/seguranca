0e5a49fdd8d49cd41d1ad2d912495e63f40a16ed
Roddas Cabral <roddas360@gmail.com>
2024-05-03 11:57:42 +0000

    Update create_user_groups.sh

--- blue-team/scripts/create_user_groups.sh
+++ blue-team/scripts/create_user_groups.sh
@@ -1,32 +1,30 @@
+  GNU nano 7.2                                            create_groups.sh                                            Modified  
 #!/usr/bin/bash
 
+
+BASE_GID=58999
+
 function create_group_folder(){
-	if ! test -d $1; then
-		sudo mkdir $1
-		echo "[+] Created the folder for $1 group"
-	fi
+        if ! test -d $1; then
+                sudo mkdir /home/$1
+                echo "[+] Created the folder for $1 group"
+        fi
 }
 function create_group(){
-	if ! getent group $1 >> /dev/null; then
-		sudo groupadd $1
-		echo "[+] Created the $1 group."
-	fi
+        BASE_GID=$((BASE_GID+1))
+        if ! getent group $1 >> /dev/null; then
+                sudo groupadd $1 -g $BASE_GID
+                create_group_folder $1
+                echo "[+] Created the $1 group."
+        fi
 }
 
-# Variables
-interns_home_path="/home/interns_group"
-equip_members_home_path="/home/equip_members_group"
-guests_home_path="/home/guest"
-admin_home_path="/home/administrator"
 
-# Create the group's folder
-create_group_folder $interns_home_path
-create_group_folder $equip_members_home_path
-create_group_folder $guests_home_path
-create_group_folder $admin_home_path
 
 # Create the proper groupds
-create_group "equip_members"
 create_group "guest"
+create_group "equip_member"
 
 echo "[+] Group of users was created successfully :-D. You may now create the users :-D"
+
+
