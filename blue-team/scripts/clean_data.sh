b8360afe89d39bcaee03139051db5bcf0ac557e0
Roddas Cabral <roddas360@gmail.com>
2024-05-06 19:42:49 +0000

    Update clean_data.sh

--- blue-team/scripts/clean_data.sh
+++ blue-team/scripts/clean_data.sh
@@ -6,8 +6,18 @@ function delete_group(){
 		echo "[+] The group $1 was successfully deleted ."
 	fi
 	if test -d /home/$1 >> /dev/null; then
-		sudo rm -rf /home/$1
+		sudo rm -rf /home/$1 2> /dev/null
 		echo "[+] Deleted the remaining folder of the $1 group."
+	fi
+}
+function delete_users(){
+	if test -d /home/$1; then
+		ls /home/$1 > /tmp/user_list
+		while read -r user
+		do
+			sudo userdel $user -r
+			echo "The user $user was deleted .."
+		done < /tmp/user_list
 	fi
 }
 
@@ -16,4 +26,11 @@ delete_group "equip_member"
 delete_group "guest"
 delete_group "administrator"
 
+delete_users root
+delete_users guest
+delete_users equip_member
+
+rm output.txt 2> /dev/null
+
 echo "[+] DONE."
+username@username:~
