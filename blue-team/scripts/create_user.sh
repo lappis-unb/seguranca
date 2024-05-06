37b14cc1e4c0ac6edd0970eba76e7d2ca35a9228
Roddas Cabral <roddas360@gmail.com>
2024-05-06 19:22:32 +0000

    update(user-access-control) : Create the root user

--- blue-team/scripts/create_user.sh
+++ blue-team/scripts/create_user.sh
@@ -15,11 +15,26 @@ function create_user(){
 function new_user(){
 	while read -r line
 	do
-		a=( $(echo $line | sed "s/PASS/$(makepasswd -c $regex)/") )
+		a=( $(echo $line | sed "s/PASS/$(makepasswd -l 15 -c $regex)/") )
 		password=${a[3]}
 		username=${a[0]}
 		group=${a[2]}
-		echo "$username:$password:$group"
+
+		if [ "$group" = "root" ] ; then
+			
+			# Creates the user wihtin the root group
+
+			sudo useradd $username -b /home/root -g root
+			echo $password  |sudo passwd $username --stdin
+			echo "The user $username was created with root privileges. Take care .."
+		elif [ "$group" = "equip_member" ] ; then
+			echo "group member"
+		elif [ "$group" = "guest" ] ; then
+			echo "guest"
+		else
+			echo "Group not recognized"
+		fi
+		echo  "$username $password $group" >> output.txt
 	done < "$input"
 }
 
