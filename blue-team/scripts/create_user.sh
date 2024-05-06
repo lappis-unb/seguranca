8db6e481c1cdc30a94a11cce999a0740f79b5e54
Roddas Cabral <roddas360@gmail.com>
2024-05-06 19:36:02 +0000

    update(user-access-control): Add the guest users and equip member users

--- blue-team/scripts/create_user.sh
+++ blue-team/scripts/create_user.sh
@@ -23,20 +23,28 @@ function new_user(){
 		if [ "$group" = "root" ] ; then
 			
 			# Creates the user wihtin the root group
-
 			sudo useradd $username -b /home/root -g root
 			echo $password  |sudo passwd $username --stdin
 			echo "The user $username was created with root privileges. Take care .."
+		
 		elif [ "$group" = "equip_member" ] ; then
-			echo "group member"
+
+			# Creates the equip member user
+			sudo useradd $username -b /home/equip_member -g equip_member
+			echo $password  | sudo passwd $username --stdin
+			echo "The user $username was created within equip member group .."
 		elif [ "$group" = "guest" ] ; then
-			echo "guest"
+			
+			# Creates the guest user
+			sudo useradd $username -b /home/guest -g guest
+			echo $password  | sudo passwd $username --stdin
+			echo "The guest user $username was created  .."
 		else
 			echo "Group not recognized"
+			exit
 		fi
 		echo  "$username $password $group" >> output.txt
 	done < "$input"
 }
 
-#create_user
 new_user
